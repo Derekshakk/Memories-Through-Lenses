2e0ec23b9ae5d770c4ffe173d52bbec8e1b69751
Derek Sha <124758196+Derekshakk@users.noreply.github.com>
2024-05-04 19:00:59 -0700

    Login Screen Initialization
    Started on the programming of the Login Page

---
+++ memories_through_lenses/lib/initialization.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:memories_through_lenses/screens/login.dart';
+
+class Initializer extends StatelessWidget {
+  const Initializer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
---
+++ memories_through_lenses/lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:memories_through_lenses/initialization.dart';
+import 'package:memories_through_lenses/screens/login.dart';
+
+var routes = {
+  "/": (context) => Initializer(),
+  "/login": (context) => LoginPage()
+};
