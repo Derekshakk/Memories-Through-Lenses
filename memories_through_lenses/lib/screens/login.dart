b2885259a449fd68dd7211a4c848420a7d16392e
Derek Sha <124758196+Derekshakk@users.noreply.github.com>
2024-05-18 19:03:27 -0700

    Checkpoint
    System check

--- memories_through_lenses/lib/screens/login.dart
+++ memories_through_lenses/lib/screens/login.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          "LOGIN",
-          style: TextStyle(fontSize: 50),
-        ),
-        TextField(),
-        TextField(),
-        ElevatedButton(onPressed: () {}, child: Text("LOGIN")),
-        TextButton(onPressed: () {}, child: Text("Forgot Password?")),
-        TextButton(onPressed: () {}, child: Text("Need an account? SIGN UP")),
-      ],
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "LOGIN",
+            style: TextStyle(fontSize: 50),
+          ),
+          TextField(controller: emailController),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("LOGIN")),
+          TextButton(onPressed: () {}, child: Text("Forgot Password?")),
+          TextButton(onPressed: () {}, child: Text("Need an account? SIGN UP")),
+        ],
+      ),
     ));
   }
 }
