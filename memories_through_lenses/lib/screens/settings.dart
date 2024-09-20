65eb48c9b5ff0dc1ffd5e5ce4ba446e94b6e583e
Andrew Park <ajpark5@uci.edu>
2024-09-19 18:15:32 -0700

    quick change

--- memories_through_lenses/lib/screens/settings.dart
+++ memories_through_lenses/lib/screens/settings.dart
@@ -25,93 +25,93 @@ class _SettingsScreenState extends State<SettingsScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(
-                  width: SizeConfig.blockSizeHorizontal! * 90,
-                  // height: SizeConfig.blockSizeVertical! * 40,
-                  child: const Card(
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Text("Notification Settings",
-                              style: TextStyle(fontSize: 20)),
-                          ToggleRow(title: "Disable All Notifications"),
-                          ToggleRow(title: "New Uploads in Groups"),
-                          ToggleRow(title: "Comments on Posts"),
-                          ToggleRow(title: "Likes on Posts"),
-                          ToggleRow(title: "New Group Invitations"),
-                          ToggleRow(title: "Friend Requests"),
-                          ToggleRow(title: "Admin Announcements"),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: SizeConfig.blockSizeHorizontal! * 90,
-                  // height: SizeConfig.blockSizeVertical! * 40,
-                  child: const Card(
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Text("Privacy Settings",
-                              style: TextStyle(fontSize: 20)),
-                          ToggleRow(title: "Private Account"),
-                          ToggleRow(title: "Hide Profile from Search"),
-                          ToggleRow(title: "Hide Friends List"),
-                          ToggleRow(title: "Hide Groups"),
-                          ToggleRow(title: "Hide Posts"),
-                          ToggleRow(title: "Hide Comments"),
-                          ToggleRow(title: "Hide Likes"),
-                          ToggleRow(title: "Hide Groups"),
-                          ToggleRow(title: "Hide Posts"),
-                          ToggleRow(title: "Hide Comments"),
-                          ToggleRow(title: "Hide Likes"),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: SizeConfig.blockSizeHorizontal! * 90,
-                  height: SizeConfig.blockSizeVertical! * 20,
-                  child: Card(
-                    color: Colors.grey,
-                    child: ListWheelScrollView(
-                      itemExtent: 50,
-                      children: [
-                        ListTile(
-                          tileColor: Colors.white,
-                          title: Text("English"),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          tileColor: Colors.white,
-                          title: Text("Spanish"),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          tileColor: Colors.white,
-                          title: Text("Japanese"),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          tileColor: Colors.white,
-                          title: Text("Chinese"),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          tileColor: Colors.white,
-                          title: Text("Korean"),
-                          onTap: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                // SizedBox(
+                //   width: SizeConfig.blockSizeHorizontal! * 90,
+                //   // height: SizeConfig.blockSizeVertical! * 40,
+                //   child: const Card(
+                //     color: Colors.grey,
+                //     child: Padding(
+                //       padding: EdgeInsets.all(16.0),
+                //       child: Column(
+                //         children: [
+                //           Text("Notification Settings",
+                //               style: TextStyle(fontSize: 20)),
+                //           ToggleRow(title: "Disable All Notifications"),
+                //           ToggleRow(title: "New Uploads in Groups"),
+                //           ToggleRow(title: "Comments on Posts"),
+                //           ToggleRow(title: "Likes on Posts"),
+                //           ToggleRow(title: "New Group Invitations"),
+                //           ToggleRow(title: "Friend Requests"),
+                //           ToggleRow(title: "Admin Announcements"),
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // SizedBox(
+                //   width: SizeConfig.blockSizeHorizontal! * 90,
+                //   // height: SizeConfig.blockSizeVertical! * 40,
+                //   child: const Card(
+                //     color: Colors.grey,
+                //     child: Padding(
+                //       padding: EdgeInsets.all(16.0),
+                //       child: Column(
+                //         children: [
+                //           Text("Privacy Settings",
+                //               style: TextStyle(fontSize: 20)),
+                //           ToggleRow(title: "Private Account"),
+                //           ToggleRow(title: "Hide Profile from Search"),
+                //           ToggleRow(title: "Hide Friends List"),
+                //           ToggleRow(title: "Hide Groups"),
+                //           ToggleRow(title: "Hide Posts"),
+                //           ToggleRow(title: "Hide Comments"),
+                //           ToggleRow(title: "Hide Likes"),
+                //           ToggleRow(title: "Hide Groups"),
+                //           ToggleRow(title: "Hide Posts"),
+                //           ToggleRow(title: "Hide Comments"),
+                //           ToggleRow(title: "Hide Likes"),
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // SizedBox(
+                //   width: SizeConfig.blockSizeHorizontal! * 90,
+                //   height: SizeConfig.blockSizeVertical! * 20,
+                //   child: Card(
+                //     color: Colors.grey,
+                //     child: ListWheelScrollView(
+                //       itemExtent: 50,
+                //       children: [
+                //         ListTile(
+                //           tileColor: Colors.white,
+                //           title: Text("English"),
+                //           onTap: () {},
+                //         ),
+                //         ListTile(
+                //           tileColor: Colors.white,
+                //           title: Text("Spanish"),
+                //           onTap: () {},
+                //         ),
+                //         ListTile(
+                //           tileColor: Colors.white,
+                //           title: Text("Japanese"),
+                //           onTap: () {},
+                //         ),
+                //         ListTile(
+                //           tileColor: Colors.white,
+                //           title: Text("Chinese"),
+                //           onTap: () {},
+                //         ),
+                //         ListTile(
+                //           tileColor: Colors.white,
+                //           title: Text("Korean"),
+                //           onTap: () {},
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // ),
                 ElevatedButton(
                     onPressed: () {
                       showDialog(
