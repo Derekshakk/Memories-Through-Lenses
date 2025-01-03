80c4e630c2db8d33f59a4e07b0d7c6087ffec309
Derek Sha <124758196+Derekshakk@users.noreply.github.com>
2025-01-02 19:07:23 -0800

    Gunicorn

--- moderation_ai/client.py
+++ moderation_ai/client.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_image_to_server(image_url):
-    url = 'http://192.168.4.29:5001/predict'
+    url = 'http://192.168.0.158:5001/predict'
     user_uid = 'KFO2QXGyTDYx84PEf18UfVJ606M2'
     image_name = '1280px-SIG_Pro_by_Augustas_Didzgalvis.jpg'
 
@@ -16,9 +16,9 @@ def send_image_to_server(image_url):
 
 def main():
     # image_path = 'test.jpg'
-    # image_url = 'https://en.wikipedia.org/wiki/Gun#/media/File:SIG_Pro_by_Augustas_Didzgalvis.jpg'
+    image_url = 'https://en.wikipedia.org/wiki/Gun#/media/File:SIG_Pro_by_Augustas_Didzgalvis.jpg'
     # image_url = 'https://firebasestorage.googleapis.com/v0/b/memories-through-lenses.appspot.com/o/posts%2FKFO2QXGyTDYx84PEf18UfVJ606M2%2F2024-09-18%2009%3A59%3A42.463890?alt=media&token=e68ced26-d2ef-4bda-90b7-6014314909b4'
-    image_url = 'https://firebasestorage.googleapis.com/v0/b/memories-through-lenses.appspot.com/o/posts%2FKFO2QXGyTDYx84PEf18UfVJ606M2%2F1280px-SIG_Pro_by_Augustas_Didzgalvis.jpg?alt=media&token=5666dc47-a116-41bd-9cec-0038d10d9f49'
+    # image_url = 'https://firebasestorage.googleapis.com/v0/b/memories-through-lenses.appspot.com/o/posts%2FKFO2QXGyTDYx84PEf18UfVJ606M2%2F1280px-SIG_Pro_by_Augustas_Didzgalvis.jpg?alt=media&token=5666dc47-a116-41bd-9cec-0038d10d9f49'
     result = send_image_to_server(image_url)
 
     if 'predictions' in result and result['predictions'] is not None:
